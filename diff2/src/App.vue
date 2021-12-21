ef5b510cb0a308bd97bdfc8e4df3b72243120a67
关注奇葩的人生 <oncwnuKQDwWI8dZS8VDtoyJTItZo@git.weixin.qq.com>
2021-12-21 17:48:09 +0800

    first-commit

---
+++ diff2/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <div class="app_router">
+    <!-- <router-link to="/">home</router-link>   
+    <router-link to="/about" style="margin-left:30px">future</router-link> -->
+    </div>
+    <router-view />
+
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+  }
+}
+</script>
+
+<style>
+@import "./assets/css/main.css";
+#app {
+  font-family: "Microsoft YaHei","Microsoft JhengHei";
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+.app_router{
+  margin: 30px auto;
+  width: 100%;
+  text-align: center;
+}
+.app_router a{
+  font-size: 2rem;
+  font-weight: bold;
+  color: rgb(172, 169, 169);
+}
+
+</style>
---
+++ diff2/src/plugins/element.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import { 
+    Button,
+    Row,
+    Col,
+
+
+} from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Row)
+Vue.use(Col)
